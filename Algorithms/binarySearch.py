b627fed62c766ddd63ed86863edf17bbdfbd5bcf
Jbnye <jbnye@umich.edu>
2025-03-21 22:42:55 -0400

    Binary search

---
+++ Algorithms/binarySearch.py
@@ -0,0 +1,22 @@
+
+
+def binarySearch (arr, target):
+    arr.sort()
+    left = 0
+    right = len(arr) - 1
+    middle = 0
+    while left <= right:
+        middle = (left + right) // 2
+        if(arr[middle] == target):
+            return True
+        elif(arr[middle] < target):
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return False
+
+nums = [3,6,1,4,3,2,7,9,2,11,34,5,4,23,54,32,0,431]
+target = 24
+ans = binarySearch(nums, target)
+print(ans)
