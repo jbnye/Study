f983778b3720b2e64e305346804155211a85fddf
Jbnye <jbnye@umich.edu>
2025-04-01 01:06:47 -0400

    added queue and stack implamentation in py

---
+++ DataStructures/Stack.py
@@ -0,0 +1,33 @@
+class Stack():
+
+    def __init__(self):
+        self.stack = []
+    
+    def pop(self):
+        if(len(self.stack) != 0):
+            stack.pop()
+        return None
+
+    def peek(stack):
+        if(len(stack) >0):
+            return stack[len(stack)-1]
+        return None
+    
+    def push(self, item):
+        self.stack.append(item)
+        return stack
+    def size(self):
+        return len(self.stack)
+    
+stack = Stack()
+
+stack.push(stack, 2)
+
+
+
+
+
+
+
+
+
